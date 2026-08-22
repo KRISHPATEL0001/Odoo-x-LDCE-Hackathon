dffd873899a3259721983ea87b80e7b5ce2268d4
KRISHPATEL0001 <krishmaheshpatel0001@gmail.com>
2026-08-22 16:51:35 +0530

    Minor code update

--- Frontend/globetrotter/vite.config.ts
+++ Frontend/globetrotter/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Odoo-x-LDCE-Hackathon/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
